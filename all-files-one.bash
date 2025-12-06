8913f9a2ee4635e50119a93b38251389b93f198a
ErickAvilaB <yahir7083@gmail.com>
2025-12-05 19:44:33 -0600

    delete all-files-one

--- all-files-one.bash
+++
@@ -1,8 +0,0 @@
-# Solo para Lex.x, Parser.y, Desugar.hs, MiniLisp.hs y Interp.hs
-# Guarda el contenido de los archivos en un archivo de texto llamado all-files-one.txt
-
-for f in Lex.x Parser.y Desugar.hs MiniLisp.hs Interp.hs; do
-    echo "=== File: $f ===" >> all-files-one.txt
-    cat "$f" >> all-files-one.txt
-    echo -e "\n" >> all-files-one.txt
-done
