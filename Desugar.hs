460e661710ba961245d26bfd85338febe4f24798
ErickAvilaB <yahir7083@gmail.com>
2025-12-05 20:03:55 -0600

    correcciones

--- Desugar.hs
+++ Desugar.hs
@@ -1,15 +1,26 @@
 module Desugar where
 
--- Importamos Parser cualificado para usar su PrimTag y constructores (Plus, Minus, ...),
--- y al mismo tiempo traer los constructores de SASA sin colisiones.
-import Parser hiding (PrimTag(..))
-import qualified Parser as P
+-- Corrección 7 en el documento:
+--  * Alineamos este módulo con el núcleo teórico del lenguaje.
+--  * El núcleo YA NO tiene operadores variádicos ni if0 como primitivas.
+--  * Los variádicos de la superficie (+ - * / = < > <= >= !=) se desazucaran
+--    aquí a combinaciones BINARIAS, y if0 se define en términos de if y = 0.
+
+-- Importamos solo el ASA de superficie (SASA) sin los constructores de PrimTag,
+-- y usamos Parser cualificado (P.) para los tags de primitivas.
+import Parser (SASA (..))
+import Parser qualified as P
 
 --------------------------------------------------------------------------------
 -- Núcleo (ASA)
 --------------------------------------------------------------------------------
 
--- ASA: core del lenguaje tras desazucarado
+-- ASA: core del lenguaje tras desazucarado.
+-- En el núcleo solo hay:
+--   * operaciones aritméticas y de comparación BINARIAS,
+--   * un único condicional If,
+--   * funciones unarias y aplicación binaria,
+--   * pares y la constante Nil.
 data ASA
   = Id String
   | Num Int
@@ -18,21 +29,30 @@ data ASA
   | Pair ASA ASA
   | Fst ASA
   | Snd ASA
-
-  -- Primitivas variádicas (se mantienen como lista en el núcleo)
-  | PrimN P.PrimTag [ASA]
-
-  | Not ASA
-  | If0 ASA ASA ASA
-  | If  ASA ASA ASA
-
-  -- El núcleo es de lambdas unarias; lambdas variádicas se currifican
-  | Fun String ASA
+  | -- Aritmética binaria
+    Add ASA ASA
+  | Sub ASA ASA
+  | Mul ASA ASA
+  | Div ASA ASA
+  | Sqrt ASA
+  | Expt ASA ASA
+  | -- Comparaciones binarias
+    Eql ASA ASA
+  | Lt ASA ASA
+  | Gt ASA ASA
+  | Leq ASA ASA
+  | Geq ASA ASA
+  | Neq ASA ASA
+  | -- Booleanas y control
+    Not ASA
+  | If ASA ASA ASA
+  | -- Funciones y aplicación
+    Fun String ASA
   | App ASA ASA
   deriving (Show)
 
 --------------------------------------------------------------------------------
--- Forma para evaluar (ASAValues)
+-- ASA evaluable (con entorno) – lo que consume el intérprete
 --------------------------------------------------------------------------------
 
 data ASAValues
@@ -43,73 +63,87 @@ data ASAValues
   | PairV ASAValues ASAValues
   | FstV ASAValues
   | SndV ASAValues
-
-  | PrimNV P.PrimTag [ASAValues]  -- variádicos listados
-
-  | NotV ASAValues
-  | If0V ASAValues ASAValues ASAValues
-  | IfV  ASAValues ASAValues ASAValues
-
-  | FunV String ASAValues
+  | -- Aritmética binaria
+    AddV ASAValues ASAValues
+  | SubV ASAValues ASAValues
+  | MulV ASAValues ASAValues
+  | DivV ASAValues ASAValues
+  | SqrtV ASAValues
+  | ExptV ASAValues ASAValues
+  | -- Comparaciones binarias
+    EqlV ASAValues ASAValues
+  | LtV ASAValues ASAValues
+  | GtV ASAValues ASAValues
+  | LeqV ASAValues ASAValues
+  | GeqV ASAValues ASAValues
+  | NeqV ASAValues ASAValues
+  | -- Booleanas y control
+    NotV ASAValues
+  | IfV ASAValues ASAValues ASAValues
+  | -- Funciones y aplicación
+    FunV String ASAValues
   | ExprV ASAValues [(String, ASAValues)]
   | ClosureV String ASAValues [(String, ASAValues)]
   | AppV ASAValues ASAValues
   deriving (Show)
 
 --------------------------------------------------------------------------------
--- Azúcar (SASA) → Núcleo (ASA)
+-- Desazúcar: SASA (superficie) → ASA (núcleo)
 --------------------------------------------------------------------------------
 
 desugar :: SASA -> ASA
--- básicos
-desugar (IdS i)                 = Id i
-desugar (NumS n)                = Num n
-desugar (BooleanS b)            = Boolean b
-
--- nil y pares / listas
-desugar NilS                    = Nil
-desugar (PairS a b)             = Pair (desugar a) (desugar b)
-desugar (FstS e)                = Fst (desugar e)
-desugar (SndS e)                = Snd (desugar e)
-desugar (ListS xs)              = foldr (\e acc -> Pair (desugar e) acc) Nil xs
-
+-- Literales y variables
+desugar (IdS i) = Id i
+desugar (NumS n) = Num n
+desugar (BooleanS b) = Boolean b
+desugar NilS = Nil
+-- Pares y listas
+desugar (PairS a b) = Pair (desugar a) (desugar b)
+desugar (FstS e) = Fst (desugar e)
+desugar (SndS e) = Snd (desugar e)
+-- Listas por corchetes: [e1, ..., en] ⇒ (e1, ...(en, nil)...)
+desugar (ListS xs) = foldr (\e acc -> Pair (desugar e) acc) Nil xs
 -- unarias especiales y not
-desugar (NotS e)                = Not (desugar e)
-desugar (Add1S e)               = PrimN P.Plus  [desugar e, Num 1]
-desugar (Sub1S e)               = PrimN P.Minus [desugar e, Num 1]
-desugar (SqrtS e)               = PrimN P.Sqrt  [desugar e]
-desugar (ExptS a b)             = PrimN P.Expt  [desugar a, desugar b]
-
--- variádicos (+ - * / = < > <= >= !=)
-desugar (PrimNS t xs)           = PrimN t (map desugar xs)
-
+desugar (NotS e) = Not (desugar e)
+-- add1/sub1 como azúcar de + / -
+desugar (Add1S e) = Add (desugar e) (Num 1)
+desugar (Sub1S e) = Sub (desugar e) (Num 1)
+desugar (SqrtS e) = Sqrt (desugar e)
+desugar (ExptS a b) = Expt (desugar a) (desugar b)
+-- Corrección 7 en el documento:
+--   Los operadores variádicos de superficie se traducen aquí a combinaciones
+--   binarias del núcleo; el intérprete SOLO ve primitivas binarias.
+desugar (PrimNS tag xs) = desugarPrimNS tag (map desugar xs)
 -- if / if0
-desugar (IfS  c t e)            = If  (desugar c) (desugar t) (desugar e)
-desugar (If0S c t e)            = If0 (desugar c) (desugar t) (desugar e)
-
--- [(SASA,SASA)] SASA donde [(c1,t1), (c2,t2), ...] es la lista de condiciones y sus cuerpos, y SASA es el else
+desugar (IfS c t e) = If (desugar c) (desugar t) (desugar e)
+-- Corrección 7 en el documento:
+--   if0 es azúcar: (if0 e t f)  ==>  (if (= e 0) t f)
+desugar (If0S c t e) =
+  let c' = desugar c
+   in If (Eql c' (Num 0)) (desugar t) (desugar e)
+-- cond: lista de cláusulas + else
+--   (cond [(c1 t1)] ... [(cn tn)] [else e0])
+--   ==> if c1 t1 (if c2 t2 (... (if cn tn e0)...))
 desugar (CondS clauses elseBody) =
-  foldr (\(c,t) acc -> If (desugar c) (desugar t) acc) (desugar elseBody) clauses
-
+  foldr
+    (\(c, t) acc -> If (desugar c) (desugar t) acc)
+    (desugar elseBody)
+    clauses
 -- let paralelo: ((lambda (x y ...) body) e1 e2 ...)
-desugar (LetManyS binds body)   =
+desugar (LetManyS binds body) =
   let (ps, es) = unzip binds
    in appMany (funMany ps (desugar body)) (map desugar es)
-
 -- let* secuencial: anidar lets de a uno
-desugar (LetStarManyS [] body)  = desugar body
-desugar (LetStarManyS ((x,e):xs) body) =
+desugar (LetStarManyS [] body) = desugar body
+desugar (LetStarManyS ((x, e) : xs) body) =
   App (Fun x (desugar (LetStarManyS xs body))) (desugar e)
-
--- letrec como en tu base, vía Z y lambda variádica singleton
-desugar (LetRecS p v c)         =
+-- letrec vía combinador Z (variable libre "Z" provista en el prelude)
+desugar (LetRecS p v c) =
   desugar (LetManyS [(p, AppManyS (IdS "Z") [FunManyS [p] v])] c)
-
 -- lambdas variádicas → currificadas
-desugar (FunManyS ps body)      = funMany ps (desugar body)
-
+desugar (FunManyS ps body) = funMany ps (desugar body)
 -- aplicación múltiple → aplicaciones anidadas
-desugar (AppManyS f args)       = appMany (desugar f) (map desugar args)
+desugar (AppManyS f args) = appMany (desugar f) (map desugar args)
 
 --------------------------------------------------------------------------------
 -- Helpers (azúcar del núcleo)
@@ -119,27 +153,85 @@ desugar (AppManyS f args)       = appMany (desugar f) (map desugar args)
 appMany :: ASA -> [ASA] -> ASA
 appMany = foldl App
 
--- (lambda (x1 x2 ... xn) body)  ==  (lambda x1 (lambda x2 ... (lambda xn body)...))
+-- (lambda (x1 x2 ... xn) body)
+--   == (lambda x1 (lambda x2 ... (lambda xn body)...))
 funMany :: [String] -> ASA -> ASA
-funMany ps body = foldr (\p acc -> Fun p acc) body ps
+funMany ps body = foldr Fun body ps
+
+--------------------------------------------------------------------------------
+-- Helpers para operadores variádicos de superficie
+--------------------------------------------------------------------------------
+
+-- Plegado izquierdo aritmético:
+--   (+ a b c)  ==>  Add (Add a b) c
+foldArith :: (ASA -> ASA -> ASA) -> [ASA] -> ASA
+foldArith _ [] = error "operador aritmético variádico sin argumentos"
+foldArith _ [_] = error "operador aritmético variádico requiere ≥ 2 argumentos"
+foldArith op (e1 : e2 : es) = foldl op (op e1 e2) es
+
+-- Comparadores en cadena con cortocircuito:
+--   (< a b c)  ==>  if (a<b) (if (b<c) #t #f) #f
+cmpChain :: (ASA -> ASA -> ASA) -> [ASA] -> ASA
+cmpChain _ [] = error "comparador variádico sin argumentos"
+cmpChain _ [_] = error "comparador variádico requiere ≥ 2 argumentos"
+cmpChain op (e1 : e2 : es) = go e1 e2 es
+  where
+    go a b [] = op a b
+    go a b (c : cs) = If (op a b) (go b c cs) (Boolean False)
+
+-- Traducción de PrimNS de superficie al núcleo binario
+desugarPrimNS :: P.PrimTag -> [ASA] -> ASA
+desugarPrimNS tag es =
+  case tag of
+    -- Aritmética variádica (plegado izquierdo)
+    P.Plus -> foldArith Add es
+    P.Minus -> foldArith Sub es
+    P.Times -> foldArith Mul es
+    P.Div -> foldArith Div es
+    -- Comparaciones variádicas encadenadas
+    P.Eql -> cmpChain Eql es
+    P.Neq -> cmpChain Neq es
+    P.Lt -> cmpChain Lt es
+    P.Gt -> cmpChain Gt es
+    P.Leq -> cmpChain Leq es
+    P.Geq -> cmpChain Geq es
+    -- sqrt y expt:
+    P.Sqrt ->
+      case es of
+        [e] -> Sqrt e
+        _ -> error "sqrt espera 1 argumento (núcleo)"
+    P.Expt ->
+      case es of
+        [b, e] -> Expt b e
+        _ -> error "expt espera 2 argumentos (núcleo)"
 
 --------------------------------------------------------------------------------
 -- Núcleo (ASA) → Forma evaluable (ASAValues)
 --------------------------------------------------------------------------------
 
 desugarV :: ASA -> ASAValues
-desugarV (Id i)          = IdV i
-desugarV (Num n)         = NumV n
-desugarV (Boolean b)     = BooleanV b
-desugarV Nil             = NilV
-desugarV (Pair a b)      = PairV (desugarV a) (desugarV b)
-desugarV (Fst e)         = FstV (desugarV e)
-desugarV (Snd e)         = SndV (desugarV e)
-
-desugarV (PrimN t xs)    = PrimNV t (map desugarV xs)
-
-desugarV (Not e)         = NotV (desugarV e)
-desugarV (If0 c t e)     = If0V (desugarV c) (desugarV t) (desugarV e)
-desugarV (If  c t e)     = IfV  (desugarV c) (desugarV t) (desugarV e)
-desugarV (Fun p c)       = FunV p (desugarV c)
-desugarV (App f a)       = AppV (desugarV f) (desugarV a)
+desugarV (Id i) = IdV i
+desugarV (Num n) = NumV n
+desugarV (Boolean b) = BooleanV b
+desugarV Nil = NilV
+desugarV (Pair a b) = PairV (desugarV a) (desugarV b)
+desugarV (Fst e) = FstV (desugarV e)
+desugarV (Snd e) = SndV (desugarV e)
+-- Aritmética / comparaciones / unarias
+desugarV (Add a b) = AddV (desugarV a) (desugarV b)
+desugarV (Sub a b) = SubV (desugarV a) (desugarV b)
+desugarV (Mul a b) = MulV (desugarV a) (desugarV b)
+desugarV (Div a b) = DivV (desugarV a) (desugarV b)
+desugarV (Sqrt e) = SqrtV (desugarV e)
+desugarV (Expt b e) = ExptV (desugarV b) (desugarV e)
+desugarV (Eql a b) = EqlV (desugarV a) (desugarV b)
+desugarV (Lt a b) = LtV (desugarV a) (desugarV b)
+desugarV (Gt a b) = GtV (desugarV a) (desugarV b)
+desugarV (Leq a b) = LeqV (desugarV a) (desugarV b)
+desugarV (Geq a b) = GeqV (desugarV a) (desugarV b)
+desugarV (Neq a b) = NeqV (desugarV a) (desugarV b)
+desugarV (Not e) = NotV (desugarV e)
+-- Control y funciones
+desugarV (If c t e) = IfV (desugarV c) (desugarV t) (desugarV e)
+desugarV (Fun p c) = FunV p (desugarV c)
+desugarV (App f a) = AppV (desugarV f) (desugarV a)
--- Interp.hs
+++ Interp.hs
@@ -1,6 +1,10 @@
 module Interp where
 
-import qualified Parser as P
+-- Corrección 6 en el documento:
+--  * El intérprete ahora solo maneja el núcleo BINARIO (sin variádicos ni if0).
+--  * La evaluación sigue la SOS de paso-pequeño, call-by-value, con evaluación
+--    estricta de izquierda a derecha, como en el documento teórico.
+
 import Desugar
 
 -- Entorno léxico
@@ -11,78 +15,195 @@ type Env = [(String, ASAValues)]
 --------------------------------------------------------------------------------
 
 smallStep :: ASAValues -> Env -> (ASAValues, Env)
-
 -- Identificadores y literales
-smallStep (IdV i) env                   = (lookupEnv i env, env)
-smallStep (NumV n) env                  = (NumV n, env)
-smallStep (BooleanV b) env              = (BooleanV b, env)
-smallStep NilV env                      = (NilV, env)
-
+smallStep (IdV i) env = (lookupEnv i env, env)
+smallStep (NumV n) env = (NumV n, env)
+smallStep (BooleanV b) env = (BooleanV b, env)
+smallStep NilV env = (NilV, env)
 -- Pares
 smallStep (PairV v w) env
-  | isValueV v && isValueV w            = (PairV v w, env)
+  | isValueV v && isValueV w = (PairV v w, env)
 smallStep (PairV v w) env
-  | isValueV v                          = let (w',_) = smallStep w env in (PairV v w', env)
-  | otherwise                           = let (v',_) = smallStep v env in (PairV v' w, env)
-
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (PairV v w', env)
+smallStep (PairV v w) env =
+  let (v', _) = smallStep v env
+   in (PairV v' w, env)
 -- Proyecciones
 smallStep (FstV p) env
   | isValueV p =
       case p of
         PairV v _ -> (v, env)
-        _         -> runtimeError "fst espera un par"
-  | otherwise   = let (p',_) = smallStep p env in (FstV p', env)
-
+        _ -> runtimeError "fst espera un par"
+smallStep (FstV p) env =
+  let (p', _) = smallStep p env
+   in (FstV p', env)
 smallStep (SndV p) env
   | isValueV p =
       case p of
         PairV _ w -> (w, env)
-        _         -> runtimeError "snd espera un par"
-  | otherwise   = let (p',_) = smallStep p env in (SndV p', env)
-
+        _ -> runtimeError "snd espera un par"
+smallStep (SndV p) env =
+  let (p', _) = smallStep p env
+   in (SndV p', env)
 -- Booleanas
 smallStep (NotV v) env
   | isValueV v =
       case v of
         BooleanV b -> (BooleanV (not b), env)
-        _          -> runtimeError "not espera booleano"
-  | otherwise   = let (v',_) = smallStep v env in (NotV v', env)
+        _ -> runtimeError "not espera booleano"
+smallStep (NotV v) env =
+  let (v', _) = smallStep v env
+   in (NotV v', env)
+--------------------------------------------------------------------------------
+-- Primitivas aritméticas y de comparación binarias
+--------------------------------------------------------------------------------
 
--- Condicionales
+-- (+ v w)
+smallStep (AddV v w) env
+  | isValueV v && isValueV w = (applyIntBin (+) v w, env)
+smallStep (AddV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (AddV v w', env)
+smallStep (AddV v w) env =
+  let (v', _) = smallStep v env
+   in (AddV v' w, env)
+-- (- v w)
+smallStep (SubV v w) env
+  | isValueV v && isValueV w = (applyIntBin (-) v w, env)
+smallStep (SubV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (SubV v w', env)
+smallStep (SubV v w) env =
+  let (v', _) = smallStep v env
+   in (SubV v' w, env)
+-- (* v w)
+smallStep (MulV v w) env
+  | isValueV v && isValueV w = (applyIntBin (*) v w, env)
+smallStep (MulV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (MulV v w', env)
+smallStep (MulV v w) env =
+  let (v', _) = smallStep v env
+   in (MulV v' w, env)
+-- (/ v w)
+smallStep (DivV v w) env
+  | isValueV v && isValueV w = (applyIntBin safeDiv v w, env)
+smallStep (DivV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (DivV v w', env)
+smallStep (DivV v w) env =
+  let (v', _) = smallStep v env
+   in (DivV v' w, env)
+-- sqrt v
+smallStep (SqrtV e) env
+  | isValueV e =
+      (applyUnaryInt (\n -> NumV (floor (sqrt (fromIntegral n :: Double)))) e, env)
+smallStep (SqrtV e) env =
+  let (e', _) = smallStep e env
+   in (SqrtV e', env)
+-- expt b e
+smallStep (ExptV b e) env
+  | isValueV b && isValueV e = (applyIntBin (^) b e, env)
+smallStep (ExptV b e) env
+  | isValueV b =
+      let (e', _) = smallStep e env
+       in (ExptV b e', env)
+smallStep (ExptV b e) env =
+  let (b', _) = smallStep b env
+   in (ExptV b' e, env)
+-- Comparadores binarios (sobre enteros)
+smallStep (EqlV v w) env
+  | isValueV v && isValueV w = (applyCmpBin (==) v w, env)
+smallStep (EqlV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (EqlV v w', env)
+smallStep (EqlV v w) env =
+  let (v', _) = smallStep v env
+   in (EqlV v' w, env)
+smallStep (NeqV v w) env
+  | isValueV v && isValueV w = (applyCmpBin (/=) v w, env)
+smallStep (NeqV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (NeqV v w', env)
+smallStep (NeqV v w) env =
+  let (v', _) = smallStep v env
+   in (NeqV v' w, env)
+smallStep (LtV v w) env
+  | isValueV v && isValueV w = (applyCmpBin (<) v w, env)
+smallStep (LtV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (LtV v w', env)
+smallStep (LtV v w) env =
+  let (v', _) = smallStep v env
+   in (LtV v' w, env)
+smallStep (GtV v w) env
+  | isValueV v && isValueV w = (applyCmpBin (>) v w, env)
+smallStep (GtV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (GtV v w', env)
+smallStep (GtV v w) env =
+  let (v', _) = smallStep v env
+   in (GtV v' w, env)
+smallStep (LeqV v w) env
+  | isValueV v && isValueV w = (applyCmpBin (<=) v w, env)
+smallStep (LeqV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (LeqV v w', env)
+smallStep (LeqV v w) env =
+  let (v', _) = smallStep v env
+   in (LeqV v' w, env)
+smallStep (GeqV v w) env
+  | isValueV v && isValueV w = (applyCmpBin (>=) v w, env)
+smallStep (GeqV v w) env
+  | isValueV v =
+      let (w', _) = smallStep w env
+       in (GeqV v w', env)
+smallStep (GeqV v w) env =
+  let (v', _) = smallStep v env
+   in (GeqV v' w, env)
+--------------------------------------------------------------------------------
+-- Condicional
+--------------------------------------------------------------------------------
+
 smallStep (IfV c t e) env
   | isValueV c =
       case c of
-        BooleanV True  -> (t, env)
+        BooleanV True -> (t, env)
         BooleanV False -> (e, env)
-        _              -> runtimeError "if espera condición booleana"
-  | otherwise   = let (c',_) = smallStep c env in (IfV c' t e, env)
+        _ -> runtimeError "if espera condición booleana"
+smallStep (IfV c t e) env =
+  let (c', _) = smallStep c env
+   in (IfV c' t e, env)
+--------------------------------------------------------------------------------
+-- Lambdas / clausuras / aplicación
+--------------------------------------------------------------------------------
 
-smallStep (If0V c t e) env
-  | isValueV c =
-      case c of
-        NumV 0 -> (t, env)
-        NumV _ -> (e, env)
-        _      -> runtimeError "if0 espera entero"
-  | otherwise   = let (c',_) = smallStep c env in (If0V c' t e, env)
-
--- Primitivas variádicas
-smallStep (PrimNV tag vs) env
-  | all isValueV vs                     = (evalPrimN tag vs, env)
-  | otherwise                           = let (vs',_) = stepFirst vs env in (PrimNV tag vs', env)
-
--- Lambdas / clausuras / aplicación (como en tu base)
-smallStep (FunV p c) env                = (ClosureV p c env, env)
-
+smallStep (FunV p c) env = (ClosureV p c env, env)
 smallStep (AppV cv@(ClosureV p b env') a) env
-  | isValueV a                          = (ExprV b ((p, a) : env'), env)
-  | otherwise                           = let (a',_) = smallStep a env in (AppV cv a', env)
-
-smallStep (AppV f a) env                = let (f',_) = smallStep f env in (AppV f' a, env)
-
--- ExprV: avanza dentro del entorno interno
+  | isValueV a = (ExprV b ((p, a) : env'), env)
+smallStep (AppV cv@(ClosureV _ _ _) a) env =
+  let (a', _) = smallStep a env
+   in (AppV cv a', env)
+smallStep (AppV f a) env =
+  let (f', _) = smallStep f env
+   in (AppV f' a, env)
+-- ExprV: avanza dentro del ambiente interno de la clausura
 smallStep (ExprV v env') env
-  | isValueV v                          = (v, env)
-  | otherwise                           = let (e1, _) = smallStep v env' in (ExprV e1 env', env)
+  | isValueV v = (v, env)
+smallStep (ExprV v env') env =
+  let (e1, _) = smallStep v env'
+   in (ExprV e1 env', env)
 
 --------------------------------------------------------------------------------
 -- Interpretación total
@@ -91,7 +212,7 @@ smallStep (ExprV v env') env
 interp :: ASAValues -> Env -> ASAValues
 interp e env
   | isValueV e = e
-  | otherwise  =
+  | otherwise =
       let (e', env') = smallStep e env
        in interp e' env'
 
@@ -100,86 +221,41 @@ interp e env
 --------------------------------------------------------------------------------
 
 isValueV :: ASAValues -> Bool
-isValueV (NumV _)           = True
-isValueV (BooleanV _)       = True
-isValueV NilV               = True
-isValueV (PairV v w)        = isValueV v && isValueV w
-isValueV (ClosureV _ _ _)   = True
-isValueV _                  = False
+isValueV (NumV _) = True
+isValueV (BooleanV _) = True
+isValueV NilV = True
+isValueV (PairV v w) = isValueV v && isValueV w
+isValueV (ClosureV _ _ _) = True
+isValueV _ = False
 
 lookupEnv :: String -> Env -> ASAValues
 lookupEnv i [] = runtimeError ("Variable " ++ i ++ " no encontrada")
 lookupEnv i ((j, v) : env)
-  | i == j    = v
+  | i == j = v
   | otherwise = lookupEnv i env
-
--- Avanza la primera subexpresión no valor de una lista de argumentos
-stepFirst :: [ASAValues] -> Env -> ([ASAValues], Env)
-stepFirst [] env = ([], env)
-stepFirst (x:xs) env
-  | isValueV x = let (xs', env') = stepFirst xs env in (x:xs', env')
-  | otherwise  = let (x', env')  = smallStep x env in (x':xs, env')
 
 runtimeError :: String -> a
 runtimeError msg = error ("Runtime error: " ++ msg)
 
 --------------------------------------------------------------------------------
--- Primitivas variádicas y helpers numéricos
+-- Helpers numéricos para el núcleo binario
 --------------------------------------------------------------------------------
 
--- Convierten y validan enteros
-nums :: [ASAValues] -> [Int]
-nums = map num
-  where
-    num (NumV n) = n
-    num _        = runtimeError "se esperaba entero"
+-- Aritmética binaria sobre enteros
+applyIntBin :: (Int -> Int -> Int) -> ASAValues -> ASAValues -> ASAValues
+applyIntBin op (NumV n1) (NumV n2) = NumV (op n1 n2)
+applyIntBin _ _ _ = runtimeError "se esperaban enteros"
+
+-- Comparadores binarios sobre enteros
+applyCmpBin :: (Int -> Int -> Bool) -> ASAValues -> ASAValues -> ASAValues
+applyCmpBin op (NumV n1) (NumV n2) = BooleanV (op n1 n2)
+applyCmpBin _ _ _ = runtimeError "comparador espera enteros"
+
+-- Unarios sobre enteros
+applyUnaryInt :: (Int -> ASAValues) -> ASAValues -> ASAValues
+applyUnaryInt f (NumV n) = f n
+applyUnaryInt _ _ = runtimeError "se esperaba entero"
 
 safeDiv :: Int -> Int -> Int
 safeDiv _ 0 = runtimeError "división por cero"
 safeDiv a b = a `div` b
-
--- Cadena de comparaciones sobre enteros
-boolChain :: (Int -> Int -> Bool) -> [ASAValues] -> ASAValues
-boolChain _ []       = BooleanV True
-boolChain _ [_]      = BooleanV True
-boolChain op vs      =
-  let ns = nums vs
-  in BooleanV (and (zipWith op ns (tail ns)))
-
-allEqual :: Eq a => [a] -> Bool
-allEqual []     = True
-allEqual (x:xs) = all (== x) xs
-
-allDistinct :: Ord a => [a] -> Bool
-allDistinct []     = True
-allDistinct (x:xs) = notElem x xs && allDistinct xs
-
--- Evaluación de primitivas
-evalPrimN :: P.PrimTag -> [ASAValues] -> ASAValues
-
--- Aritmética (+ - * /) sobre enteros (≥ 2 args)
-evalPrimN P.Plus  vs = NumV (foldl1 (+) (nums vs))
-evalPrimN P.Minus vs = NumV (foldl1 (-) (nums vs))
-evalPrimN P.Times vs = NumV (foldl1 (*) (nums vs))
-evalPrimN P.Div   vs = NumV (foldl1 safeDiv (nums vs))
-
--- sqrt (exactamente 1 arg)
-evalPrimN P.Sqrt [v]   = NumV (floor (sqrt (fromIntegral (head (nums [v])) :: Double)))
-evalPrimN P.Sqrt _     = runtimeError "sqrt espera 1 argumento"
-
--- expt (exactamente 2 args)
-evalPrimN P.Expt [a,b] =
-  case nums [a,b] of
-    [x,y] -> NumV (x ^ y)
-    _     -> runtimeError "expt espera 2 argumentos"
-evalPrimN P.Expt _     = runtimeError "expt espera 2 argumentos"
-
--- Comparaciones variádicas (≥ 2), cadena
-evalPrimN P.Lt  vs = boolChain (<)  vs
-evalPrimN P.Gt  vs = boolChain (>)  vs
-evalPrimN P.Leq vs = boolChain (<=) vs
-evalPrimN P.Geq vs = boolChain (>=) vs
-
--- Igualdad / desigualdad variádicas (≥ 2), enteros
-evalPrimN P.Eql vs = BooleanV (allEqual (nums vs))
-evalPrimN P.Neq vs = BooleanV (allDistinct (nums vs))
